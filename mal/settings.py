63504298a119c507c50ab196e2576243266eda53
Theodore Keloglou <zf@sirodoht.com>
2020-03-22 23:00:27 +0000

    enable whitenoise compressed

--- mal/settings.py
+++ mal/settings.py
@@ -121,7 +121,7 @@ USE_TZ = False
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "_static")
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Email
