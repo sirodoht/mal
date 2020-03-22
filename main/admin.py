7d1e2bde13636c1d491e9d492ee9d4614f8731a1
Theodore Keloglou <zf@sirodoht.com>
2020-03-22 19:10:36 +0000

    add user and document models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from main import models
+
+
+class Admin(UserAdmin):
+    list_display = ("id", "username", "email", "date_joined", "last_login")
+
+
+admin.site.register(models.User, Admin)
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+
+
+admin.site.register(models.Document, DocumentAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    about = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Document(models.Model):
+    title = models.CharField(max_length=300)
+    body = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
