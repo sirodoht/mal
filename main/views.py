3006123f6625555fa9b1088f0f300e6e2d934cee
Theodore Keloglou <zf@sirodoht.com>
2020-03-22 19:11:53 +0000

    add index and signup views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from main import forms
+
+
+def index(request):
+    return render(request, "main/index.html")
+
+
+@login_required
+def profile(request):
+    return render(request, "main/profile.html")
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect("main:profile")
+
+    if request.method == "POST":
+        form = forms.UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("main:login")
+    else:
+        form = forms.UserCreationForm()
+    return render(request, "registration/signup.html", {"form": form})
