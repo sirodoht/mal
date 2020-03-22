ceca24b631d2828203dc42b32b6b2f7c6241d216
Theodore Keloglou <zf@sirodoht.com>
2020-03-22 22:16:42 +0000

    add another domain

--- mal/settings.py
+++ mal/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "nonrandom_secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get("NODEBUG") is None else False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "mal.projects.sirodoht.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "mal.projects.sirodoht.com", "mostlyoverstatements.com"]
 
 
 # Application definition
