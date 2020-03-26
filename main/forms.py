a0cc9c65cc4f5d8825e700191e4ea13115abae09
Theodore Keloglou <zf@sirodoht.com>
2020-03-26 00:06:59 +0000

    add document creation form

--- main/forms.py
+++ main/forms.py
@@ -1,17 +1,26 @@
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import (
     UserChangeForm as DjUserChangeForm,
     UserCreationForm as DjUserCreationForm,
 )
 
+from main import models
+
 
 class UserChangeForm(DjUserChangeForm):
     class Meta:
         model = get_user_model()
-        fields = ("username", "email")
+        fields = ["username", "email"]
 
 
 class UserCreationForm(DjUserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = ("username",)
+        fields = ["username"]
+
+
+class DocumentCreationForm(forms.ModelForm):
+    class Meta:
+        model = models.Document
+        fields = ["title", "body"]
