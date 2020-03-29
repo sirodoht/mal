754ded8ef28fc30bedf22cbe54994d077b0bb4c3
Theodore Keloglou <zf@sirodoht.com>
2020-03-29 23:59:36 +0100

    remove user change form

--- main/forms.py
+++ main/forms.py
@@ -1,17 +1,8 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import (
-    UserChangeForm as DjUserChangeForm,
-    UserCreationForm as DjUserCreationForm,
-)
+from django.contrib.auth.forms import UserCreationForm as DjUserCreationForm
 
 from main import models
-
-
-class UserChangeForm(DjUserChangeForm):
-    class Meta:
-        model = get_user_model()
-        fields = ["username", "email"]
 
 
 class UserCreationForm(DjUserCreationForm):
