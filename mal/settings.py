89f58e90cb7cb86e8dfbec190b7698efdb827556
Theodore Keloglou <zf@sirodoht.com>
2020-04-08 21:43:38 +0100

    change smtp host

--- mal/settings.py
+++ mal/settings.py
@@ -127,7 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "_static")
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
-EMAIL_HOST = "email-smtp.eu-west-1.amazonaws.com"
+EMAIL_HOST = "smtp.postmarkapp.com"
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
