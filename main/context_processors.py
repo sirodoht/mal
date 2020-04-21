67a11cddae2faf891d109c79c943c64f7c64b8a0
Theodore Keloglou <zf@sirodoht.com>
2020-04-21 20:55:03 +0100

    add order by title on sidebar docs

--- main/context_processors.py
+++ main/context_processors.py
@@ -3,6 +3,6 @@ from main import models
 
 def sidebar(request):
     return {
-        "documents": models.Document.objects.all(),
+        "documents": models.Document.objects.all().order_by("title"),
         "featured": models.Document.objects.filter(is_featured=True),
     }
